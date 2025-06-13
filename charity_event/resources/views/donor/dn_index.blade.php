a8277026222f2c2e7109a1e0aa045fd46d2db496
TrangPii <22010386@st.phenikaa-uni.edu.vn>
2025-06-13 10:08:37 +0700

    update code

--- charity_event/resources/views/donor/dn_index.blade.php
+++ charity_event/resources/views/donor/dn_index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>🌱 HY VỌNG - Người quyên góp</title>
-    <link rel="stylesheet" href="style/donor.css">
+    <link rel="stylesheet" href="{{ asset('css/donor.css') }}">
 </head>
 <body>
     <header>
